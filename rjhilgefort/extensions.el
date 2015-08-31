1af052fe6a562c72d296664d1e358870d9445408
Rob Hilgefort <rjhilgefort@gmail.com>
2015-08-30 22:19:04 -0400

    cleaned out private layer entension

--- rjhilgefort/extensions.el
+++ rjhilgefort/extensions.el
@@ -1,24 +1,14 @@
 ;;; extensions.el --- rjhilgefort Layer extensions File for Spacemacs
-;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
 
 (setq rjhilgefort-pre-extensions
-      '(
-        ;; pre extension names go here
-        ))
+    '(
+    ;; pre extension names go here
+    ))
 
 (setq rjhilgefort-post-extensions
-      '(
-        ;; post extension names go here
-        ))
+    '(
+    ;; post extension names go here
+    ))
 
 ;; For each extension, define a function rjhilgefort/init-<extension-name>
 ;;
