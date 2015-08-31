ffd8127e85c7598be9765fa1f2fa6c5b33d15d8e
Rob Hilgefort <rjhilgefort@gmail.com>
2015-08-30 22:19:28 -0400

    Started setting up private config

--- rjhilgefort/packages.el
+++ rjhilgefort/packages.el
@@ -1,24 +1,72 @@
 ;;; packages.el --- rjhilgefort Layer packages File for Spacemacs
-;;
-;; Copyright (c) 2012-2014 Sylvain Benner
-;; Copyright (c) 2014-2015 Sylvain Benner & Contributors
-;;
-;; Author: Sylvain Benner <sylvain.benner@gmail.com>
-;; URL: https://github.com/syl20bnr/spacemacs
-;;
-;; This file is not part of GNU Emacs.
-;;
-;;; License: GPLv3
 
 ;; List of all packages to install and/or initialize. Built-in packages
 ;; which require an initialization must be listed explicitly in the list.
 (setq rjhilgefort-packages
     '(
-      ;; package names go here
+      css-mode
+      flycheck
+      grunt
+      js-doc
+      js2-mode
+      pretty-mode
+      vlf
+      vimrc-mode
+      ws-butler
+      yaml-mode
       ))
 
 ;; List of packages to exclude.
 (setq rjhilgefort-excluded-packages '())
+
+(defun rjhilgefort/init-css-mode ()
+    (use-package css-mode
+        :defer t
+        :config (setq-default css-indent-offset 2)
+        )
+    )
+
+(defun rjhilgefort/init-flycheck ()
+    (use-package flycheck
+        :config (global-flycheck-mode)))
+
+(defun oppenlander/init-js2-mode ()
+    (use-package js2-mode
+        :defer t
+        :config
+        (progn
+            (setq-default
+                js-indent-level 4
+                js2-basic-offset 4
+                js2-strict-inconsistent-return-warning nil
+                js2-mode-hide-comments t
+
+                js2-enter-indents-newline t
+                js2-bounce-indent-p t
+                js2-include-jslint-globals t
+                js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "process" "setImmediate" "exports" "enum" "it" "describe")
+                ;; Let Flycheck handle errors until js2 mode supports ES6
+                js2-show-parse-errors nil
+                js2-strict-missing-semi-warning nil
+                js2-strict-trailing-comma-warning t
+                js2-include-node-externs t
+                js2-indent-switch-body t
+                )
+            )
+        )
+    )
+
+
+
+(defun rjhilgefort/init-vimrc-mode ()
+    (use-package vimrc-mode :defer t)
+    )
+
+(defun oppenladner/init-ws-butler ()
+    (use-package ws-butler
+        :init (add-hook 'prog-mode-hook 'ws-butler-mode)
+        )
+    )
 
 ;; For each package, define a function rjhilgefort/init-<package-name>
 ;;
