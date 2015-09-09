81313094a351201e7e08b55be69e183fdecfb278
Rob Hilgefort <rjhilgefort@gmail.com>
2015-09-09 11:05:17 -0400

    formatting

--- rjhilgefort/packages.el
+++ rjhilgefort/packages.el
@@ -2,7 +2,7 @@
 
 ;; List of all packages to install and/or initialize. Built-in packages
 (setq rjhilgefort-packages
-    '(
+      '(
         css-mode
         ember-mode
         flycheck
@@ -17,64 +17,58 @@
         vimrc-mode
         ws-butler
         yaml-mode
-        )
-    )
+        ))
 
 ;; List of packages to exclude.
 (setq rjhilgefort-excluded-packages '())
 
 (defun rjhilgefort/init-css-mode ()
-    (use-package css-mode
-        :defer t
-        :config (setq-default css-indent-offset 2)
-        )
-    )
+  (use-package css-mode
+    :defer t
+    :config
+    (setq-default
+     css-indent-offset 2
+     )))
 
 (defun rjhilgefort/init-flycheck ()
-    (use-package flycheck
-        :config (global-flycheck-mode)))
+  (use-package flycheck
+    :config (global-flycheck-mode)
+    ))
 
 (defun rjhilgefort/init-js2-mode ()
-    (use-package js2-mode
-        :defer t
-        :config
-        (progn
-            (setq-default
-                js-indent-level 4
-                js2-basic-offset 4
-                js2-strict-inconsistent-return-warning nil
-                js2-mode-hide-comments t
-
-                js2-enter-indents-newline t
-                js2-bounce-indent-p t
-                js2-include-jslint-globals t
-                js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "process" "setImmediate" "exports" "enum" "it" "describe")
-                ;; Let Flycheck handle errors until js2 mode supports ES6
-                js2-show-parse-errors nil
-                js2-strict-missing-semi-warning nil
-                js2-strict-trailing-comma-warning t
-                js2-include-node-externs t
-                js2-indent-switch-body t
-                )
-            )
-        )
-    )
-
-
+  (use-package js2-mode
+    :defer t
+    :config
+    (progn
+      (setq-default
+       js-indent-level 4
+       js2-basic-offset 4
+       js2-strict-inconsistent-return-warning nil
+       js2-mode-hide-comments t
+       s2-enter-indents-newline t
+       js2-bounce-indent-p t
+       js2-include-jslint-globals t
+       js2-global-externs '("module" "require" "jQuery" "$" "_" "buster" "sinon" "assert" "refute" "setTimeout" "clearTimeout" "setInterval" "clearInterval" "location" "__dirname" "console" "JSON" "process" "setImmediate" "exports" "enum" "it" "describe")
+       ;; Let Flycheck handle errors until js2 mode supports ES6
+       js2-show-parse-errors nil
+       js2-strict-missing-semi-warning nil
+       js2-strict-trailing-comma-warning t
+       js2-include-node-externs t
+       js2-indent-switch-body t
+       ))))
 
 (defun rjhilgefort/init-vimrc-mode ()
-    (use-package vimrc-mode :defer t)
-    )
+  (use-package vimrc-mode :defer t))
 
 (defun rjhilgefort/init-ws-butler ()
-    (use-package ws-butler
-        :init (add-hook 'prog-mode-hook 'ws-butler-mode)
-        )
-    )
+  (use-package ws-butler
+    :init
+    (add-hook 'prog-mode-hook 'ws-butler-mode)
+    ))
 
 (defun rjhilgefort/init-yml-mode ()
-    (use-package yml-mode :defer t)
-    )
+  (use-package yml-mode :defer t))
+
 
 ;; For each package, define a function rjhilgefort/init-<package-name>
 ;;
